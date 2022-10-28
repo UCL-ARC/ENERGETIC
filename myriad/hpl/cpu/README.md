60422f3b7a6fda4d08280854259adb611b18bf81
David Stansby <dstansby@gmail.com>
2022-10-28 18:42:25 +0100

    Update CPU info

--- myriad/hpl/cpu/README.md
+++ myriad/hpl/cpu/README.md
@@ -6,7 +6,8 @@ Steps to run:
 
 1. Copy `hpl-2.3.tar.gz` to myriad and extract the files
 2. Copy `Make.Linux_Intel64` to the extracted directory
-3. Copy `submit_hpl.sh` to myriad, and modify the following lines:
-    - `export HPLDIR="$HOME/ENERGETIC/hpl-2.3"` to point to the extracted directory.
-    - `export UCLID=` to set your UCL username.
-4. Submit the job using `qsub submit_hpl.sh`
+3. Run `make`
+4. Copy `submit_hpl.sh` to myriad, and modify the following lines:
+    - `HPLDIR="$HOME/ENERGETIC/hpl-2.3"` to point to the extracted directory.
+5. Copy `HPL.dat` to `bin/Linux_Intel64`
+6. Submit the job using `qsub submit_hpl.sh`
--- myriad/hpl/cpu/submit_hpl.sh
+++ myriad/hpl/cpu/submit_hpl.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -l
 
-export UCLID=
-export HPLDIR="$HOME/ENERGETIC/hpl-2.3"
-
 # Request wallclock time (format hours:minutes:seconds).
 #$ -l h_rt=0:10:0
+
 # Request RAM (must be an integer followed by M, G, or T)
 #$ -l mem=1G
+
 # Request TMPDIR space (default is 10 GB - remove if cluster is diskless)
 #$ -l tmpfs=10G
 
@@ -16,15 +15,32 @@ export HPLDIR="$HOME/ENERGETIC/hpl-2.3"
 # Set the name of the job.
 #$ -N HPL_tests
 
+# Only run on one type of node
+# See https://www.rc.ucl.ac.uk/docs/Clusters/Myriad/#node-types for node documentation
+#
+#$ -ac allow=H
+
 # Set the working directory to somewhere in your scratch space.
 # This is a necessary step as compute nodes cannot write to $HOME.
-#$ -wd /home/$UCLID/Scratch
-# Your work should be done in $TMPDIR
-export ARCH=Linux_Intel64
+#$ -wd $HOME/Scratch
 
+# Do work in a temporary directory
 cd $TMPDIR
+ARCH="Linux_Intel64"
+HPLDIR="$HOME/ENERGETIC/cpu/hpl-2.3"
+CPU_POWER_OUTPUT_FILE="cpu_rapl_power_cadence.txt"
+COUNTER="/sys/class/powercap/intel-rapl/intel-rapl:0/energy_uj"
+
+# Copy HPL to temporary directory, and build
 cp -r $HPLDIR .
 cd "hpl-2.3"
 make arch=$ARCH
 cd "bin/$ARCH"
+
+# Run
+echo $(cat $COUNTER) >> $CPU_POWER_OUTPUT_FILE
 mpirun -np 4 "$(pwd)/xhpl"
+echo $(cat $COUNTER) >> $CPU_POWER_OUTPUT_FILE
+
+# Move output file
+mv -f $CPU_POWER_OUTPUT_FILE "$HOME/ENERGETIC/cpu/measurements"
