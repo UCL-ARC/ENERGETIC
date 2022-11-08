df2cd2569b20947c1a00c46ae4142f9f6beb986d
David Stansby <dstansby@gmail.com>
2022-11-08 17:22:23 +0000

    Update NVIDIA shell script to sample power

--- nextgenio/HPL/NVIDIA/run_nvidia.sh
+++ nextgenio/HPL/NVIDIA/run_nvidia.sh
@@ -1,3 +1,12 @@
+#!/usr/bin/env bash
+POWER_CADENCE_MS=100
+GPU_POWER_FILE=gpu_power.csv
+
+rm $GPU_POWER_FILE
+
+nvidia-smi --query-gpu=power.draw --format=csv --loop-ms=$POWER_CADENCE_MS --filename=$GPU_POWER_FILE &
+NVIDIA_SMI_PID=$!
+
 mpirun -n 4 \
     singularity run --nv \
     -B "./hpl_dat:/my-dat-files" \
@@ -7,3 +16,5 @@ mpirun -n 4 \
     --cpu-affinity +0:+1:+2:+3 \
     --gpu-affinity 0:0:0:0 \
     --cpu-cores-per-rank 1
+
+kill $NVIDIA_SMI_PID
