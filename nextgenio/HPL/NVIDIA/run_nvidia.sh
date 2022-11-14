63b8b905be9215540788ba0037f38381657bd197
David Stansby <dstansby@gmail.com>
2022-11-14 13:32:15 +0000

    Fix CPU sampling

--- nextgenio/HPL/NVIDIA/run_nvidia.sh
+++ nextgenio/HPL/NVIDIA/run_nvidia.sh
@@ -6,10 +6,12 @@ export CPU_POWER_OUTPUT_DIR="./power_obs"
 rm $GPU_POWER_FILE
 
 # Start sampling GPU
+echo "Sampling GPU power..."
 nvidia-smi --query-gpu=power.draw --format=csv --loop-ms=$POWER_CADENCE_MS --filename=$GPU_POWER_FILE &
 NVIDIA_SMI_PID=$!
+
 # Start sampling CPU
-../../sample_power.sh
+../../sample_power.sh &
 CPU_SAMPLE_PID=$!
 
 mpirun -n 4 \
